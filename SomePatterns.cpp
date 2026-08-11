7049ccbed3fa79c4c68227b5e99b3083a921d51d
lakshwritescode <lakshaygoel1342004@gmail.com>
2026-08-11 21:33:11 +0530

    pyramid patterns

--- SomePatterns.cpp
+++ SomePatterns.cpp
@@ -73,6 +73,56 @@ void downTriangleNums(int n)
     }
 }
 
+void pyramid(int n)
+{
+    for(int i = 0 ; i< n ; i++)
+    {
+        for(int j = 0; j <= n-i-1 ; j++)
+        {
+            cout <<" ";
+        }
+
+        for(int k = 0; k < 2*i+1 ;k++)
+        {
+            cout <<"*";
+        }
+        for (int l = 0; l <= n - i - 1; l++)
+        {
+            cout <<" ";
+        }
+
+        cout<<endl;
+    }
+}
+
+void downPyramid(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i ; j++)
+        {
+            cout <<" ";
+        }
+
+        for (int k = 0; k < 2*n - (2*i +1); k++)
+        {
+            cout <<"*";
+        }
+        for (int l = 0; l <= i; l++)
+        {
+            cout <<" ";
+        }
+
+        cout << endl;
+    }
+}
+
+void diamond(int n)
+{
+    pyramid(n);
+    downPyramid(n);
+}
+
 int main()
 {
     cout<<"give input for pattern"<<endl;
@@ -93,7 +143,13 @@ int main()
    
     // downTriangle(n);
 
-    downTriangleNums(n);
+    // downTriangleNums(n);
+
+    //pyramid(n);
+
+    //downPyramid(n);
+
+    diamond(n);
 
     return 0;
 }
