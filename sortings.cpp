6d89dcbc97a9c4a303f4c06fcf0636b8abb0be4b
lakshwritescode <lakshaygoel1342004@gmail.com>
2026-08-24 18:06:51 +0530

    practise

---
+++ sortings.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection(int arr[] , int n )
+{
+    for(int i = 0 ; i < n-1 ; i++) // n=6 , 5 rounds
+    {
+        int min = i;
+        for(int j = i+1 ; j< n ; j++)
+        {
+            if(arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+
+        swap(arr[min] , arr[i]);
+    }
+}
+
+void bubble(int arr[], int n)
+{
+    for(int i = 0 ; i< n-1;i++)
+    {
+        for(int j = 0 ; j < n-i-1 ; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                swap(arr[j] , arr[j+1]);
+            }
+        }
+    }
+}
+
+void recursiveBubble(int arr[], int n)
+{
+    if(n==1) return ; 
+
+
+    for (int j = 0; j < n- 1; j++)
+    {
+        if (arr[j] > arr[j + 1])
+        {
+            swap(arr[j], arr[j + 1]);
+        }
+    }
+
+    recursiveBubble(arr , n-1);
+}
+
+int main()
+{
+    int n;
+    n = 6;
+    int arr[n] = {13 , 46, 24 , 52 , 20 , 9};
+
+    recursiveBubble(arr , n);
+
+    for(int i = 0 ; i<n ; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
