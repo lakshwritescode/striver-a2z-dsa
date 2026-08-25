422b0ffffd7fd1b000bbe0a66933194d062b5291
lakshwritescode <lakshaygoel1342004@gmail.com>
2026-08-25 23:07:29 +0530

    practising

--- sortings.cpp
+++ sortings.cpp
@@ -32,6 +32,20 @@ void bubble(int arr[], int n)
     }
 }
 
+void insertion(int n, int arr[])
+{
+    for (int i = 0; i < n; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
+            j--;
+        }
+    }
+}
+
+
 void recursiveBubble(int arr[], int n)
 {
     if(n==1) return ; 
