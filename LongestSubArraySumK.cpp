692c60d8609e36ea3f4c9420d688912854ef8148
lakshwritescode <lakshaygoel1342004@gmail.com>
2026-09-17 19:29:29 +0530

    longest subarray with sum k optimal solution

--- LongestSubArraySumK.cpp
+++ LongestSubArraySumK.cpp
@@ -1,5 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int OptimallongestSubarrayWithSumK(vector<int> a, long long k)
+{
+#include <bits/stdc++.h>
+
+    int left = 0;
+    int right = 0;
+    long long sum = a[0];
+
+    int maxLen = 0;
+    int n = a.size();
+
+    while (right < n)
+    {
+        while (left <= right && sum > k)
+        {
+            sum -= a[left];
+            left++;
+        }
+
+        if (sum == k)
+        {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        right++;
+        if (right < n)
+            sum += a[right];
+    }
+    return maxLen;
+}
 int longestSubarrayWithSumK(vector<int> a, long long k) //better solution
 {
     map<long long, int> preSumMap;
